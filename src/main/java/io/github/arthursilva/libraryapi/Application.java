5c6df54343e396100c6a01d65f1a66bac63f2a24
Arthur Silva <arthur.gbs.dev@gmail.com>
2026-01-28 13:44:13 -0300

    Salvando um metodo de exemplo pra salvar um dado (Autor), no banco de dados

--- src/main/java/io/github/arthursilva/libraryapi/Application.java
+++ src/main/java/io/github/arthursilva/libraryapi/Application.java
@@ -1,13 +1,28 @@
 package io.github.arthursilva.libraryapi;
 
+import io.github.arthursilva.libraryapi.model.Autor;
+import io.github.arthursilva.libraryapi.repository.AutorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		var context = SpringApplication.run(Application.class, args);
+        AutorRepository repository = context.getBean(AutorRepository.class);
+        exemploSalvarRegisto(repository);
 	}
 
+    public static void exemploSalvarRegisto(AutorRepository autorRepository) {
+        Autor autor = new Autor();
+        autor.setNome("José");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of(1950, 1, 31));
+
+        var autorSalvo = autorRepository.save(autor);
+        System.out.println("Autor Salvo: " + autorSalvo);
+    }
 }
--- src/main/java/io/github/arthursilva/libraryapi/model/Autor.java
+++ src/main/java/io/github/arthursilva/libraryapi/model/Autor.java
@@ -3,6 +3,7 @@ package io.github.arthursilva.libraryapi.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -10,7 +11,7 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "autor")
-@Getter @Setter
+@Getter @Setter @ToString
 public class Autor {
 
     @Id
