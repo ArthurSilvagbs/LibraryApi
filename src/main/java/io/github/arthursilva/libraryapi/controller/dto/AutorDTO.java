0632008d565e39b8944707dd87e572586c99458c
Arthur Silva <arthur.gbs.dev@gmail.com>
2026-02-06 08:48:34 -0300

    salvando

--- src/main/java/io/github/arthursilva/libraryapi/controller/dto/AutorDTO.java
+++ src/main/java/io/github/arthursilva/libraryapi/controller/dto/AutorDTO.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
-import org.aspectj.bridge.IMessage;
 
 import java.time.LocalDate;
 import java.util.UUID;
